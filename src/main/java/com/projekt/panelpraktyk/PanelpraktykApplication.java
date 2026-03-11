aaf05bd04d2eb2a2f403b1510e07ef235eb8e793
PIZAMKADEV <pizamadev@gmail.com>
2026-03-11 09:19:02 +0100

    Commit

---
+++ src/main/java/com/projekt/panelpraktyk/PanelpraktykApplication.java
@@ -0,0 +1,13 @@
+package com.projekt.panelpraktyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelpraktykApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanelpraktykApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekt/panelpraktyk/PanelpraktykApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.panelpraktyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanelpraktykApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
